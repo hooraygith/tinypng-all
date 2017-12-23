f0cf0cecc715b162c5c3146f1144cc47ec819a68
nobody <nobody@example.com>
2017-12-23 17:29:37 +0800

    Update 流程跑通

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const glob = require('glob')
+/*
+ * 参数：
+ * -c 进程数，默认是cpu线程数
+ * -l 重复压缩的阈值，压缩率低于这个值时判断为重复压缩，默认0.2
+ * --keep  保留原文件flag，默认会替换原文件
+ */
+
+
 const {fork} = require('child_process')
 
 const readdir = require('./readdir.js')
@@ -10,49 +17,81 @@ const uploaders = Array.apply(null, Array(childCount)).map(_ => {
     return fork('./uploader.js')
 })
 
-readdir('img', {
-    match: /\.(png|jpe?g)$/i,
-    exclude: ['node_modules']
-}).then(files => {
+
+;(async () => {
+    let files = await readdir('img', {
+        match: /\.(png|jpe?g)$/i,
+        exclude: ['node_modules']
+    })
 
     let processedCount = 0
 
     uploaders.forEach(uploader => {
-        if (files[processedCount]) {
-            const file = {
-                file: files[processedCount],
-                index: processedCount
+        if (files.length) {
+            const file = findFile(files)
+            if (file) {
+                files[file.index].padding = true
+                uploader.send({file, key: findKey(keys)})
             }
-            uploader.send({file, findKey(keys)})
-            processedCount += 1
 
             uploader.on('message', val => {
                 // 结束，分配下一个
                 if (val.type === 'end') {
-                    // 如果还有文件
-                    if (files[processedCount]) {
-                        const file = {
-                            file: files[processedCount],
-                            index: processedCount
-                        }
-                        uploader.send({file, findKey(keys)})
-                        processedCount += 1
+                    const fileIndex = val.value.file.index
+                    files[fileIndex].resolved = true
+
+                    const file = findFile(files)
+                    if (file) {
+                        files[file.index].padding = true
+                        uploader.send({file, key: findKey(keys)})
+                    }
+
+                    // 如果没有可以压缩的了，结束这个进程
+                    if (files.every(file => file.resolved)) {
+                        uploader.exit()
                     }
                 }
                 // key额度用光，换下一个key，重试
                 if (val.type === 'out') {
                     const keyIndex = val.value.key.index
+                    const fileIndex = val.value.file.index
                     keys[keyIndex].invalid = true
+                    files[fileIndex].padding = false
+
+                    const file = findFile(files)
+                    if (file) {
+                        files[file.index].padding = true
+                        uploader.send({file, key: findKey(keys)})
+                    }
                 }
                 // 请求出错，重试
                 if (val.type === 'error') {
-
+                    const fileIndex = val.value.file.index
+                    files[fileIndex].padding = false
+                    const file = findFile(files)
+                    if (file) {
+                        files[file.index].padding = true
+                        uploader.send({file, key: findKey(keys)})
+                    }
                 }
             })
         }
     })
-})
+})()
 
+
+function findFile(files) {
+    for (let index = 0; index < files.length; index++) {
+        const item = files[index]
+        if (!item.resolved && !item.padding) {
+            return {
+                file: item.fullname,
+                index,
+                size: item.size
+            }
+        }
+    }
+}
 
 function findKey(keys) {
     for (let index = 0; index < keys.length; index++) {
--- uploader.js
+++ uploader.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 // file: {file, index}
 // key: {key, index}
 function compress(file, key) {
-    tinify.key = key.key
+    tinify.key = key.key.key
 
     fs.readFile(file.file, function(err, sourceData) {
         if (err) throw err
@@ -19,14 +19,28 @@ function compress(file, key) {
             // 如果出错了
             // process.send({type: 'error', value: {file, key}})
 
-            // 压缩超过30%
-            if ((file.file.size - resultData.size) / file.file.size > 0.3) {
+            // 压缩超过20%
+            if ((file.size - resultData.byteLength) / file.size > 0.2) {
 
+                let compressedName
+                if (1) {
+                    compressedName = file.file
+                } else {
+                    compressedName = file.file.replace(/(.+)(\.[^\.]+)$/i, (match, p1, p2) => {
+                        return p1 + '-comped' + p2
+                    })
+                }
 
-                // 覆盖文件
-                // fs.writeFile('')
-
-                process.send({type: 'end'})
+                fs.writeFile(compressedName, resultData, 'binary', function(err) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log(`${file.file}  压缩成功`)
+                        process.send({type: 'end', value: {file, key}})
+                    }
+                })
+            } else {
+                console.log(`${file.file}  重复压缩`)
             }
         })
     })
