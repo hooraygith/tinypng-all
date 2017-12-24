a5b00e8aba24cd4d854a7fe2f5fdf61f53f8838a
nobody <nobody@example.com>
2017-12-24 14:51:23 +0800

    Add --help

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
 /*
  * 参数：
- * -d 待压缩图片的文件夹位置
- * -c 4 进程数，默认是cpu线程数
- * -l 0.2 重复压缩的阈值，压缩率低于这个值时判断为重复压缩，默认0.2
- * -p http://127.0.0.1:1080 代理地址
- * -k true 保留原文件flag，默认会替换原文件
+ * -d --dir 待压缩图片的文件夹位置
+ * --parallel 4 进程数，默认是cpu线程数
+ * --limit 0.2 重复压缩的阈值，压缩率低于这个值时判断为重复压缩，默认0.2
+ * --proxy http://127.0.0.1:1080 代理地址
+ * --keep true 保留原文件flag，默认会替换原文件
+ * --exclude build,bin 排除的文件夹名字
  */
 
 const {fork} = require('child_process')
-const argv = require('yargs').argv;
+const _argv = require('yargs')
 
 const readdir = require('./readdir.js')
 const keys = require('./key.json').keys.map(key => ({key: key}))
 
+const argv = _argv
+    .option('dir', {
+        alias: 'd',
+        default: '',
+        desc: 'the images directory'
+    })
+    .option('parallel', {
+        default: require('os').cpus().length,
+        desc: 'parallel number, the number of child processes'
+    })
+    .option('limit', {
+        default: 0.2,
+        desc: 'the threshold that has been compressed or not'
+    })
+    .option('proxy', {
+        default: '',
+        desc: 'proxy, support http but not socks5'
+    })
+    .option('keep', {
+        default: false,
+        desc: 'weather keep the original image'
+    })
+    .option('exclude', {
+        default: '',
+        desc: 'the directory excluded'
+    })
+    .argv
+
 const config = {
     dir: argv.d,
-    childCount: argv.c || require('os').cpus().length,
-    limit: argv.l || 0.2,
-    proxy: argv.p,
-    keepFile: argv.k || false
+    childCount: argv.parallel,
+    limit: argv.limit,
+    proxy: argv.proxy,
+    keepFile: argv.keep,
+    exclude: argv.exclude
 }
 
 
-
-
 ;(async () => {
+    let exclude = ['node_modules', 'dist']
+    if (config.exclude) {
+        exclude = exclude.concat(config.exclude.split(','))
+    }
     let files = await readdir(config.dir, {
         match: /\.(png|jpe?g)$/i,
-        exclude: ['node_modules']
+        exclude
     })
 
     console.log('待处理图片数量：' + files.length)
@@ -63,8 +95,8 @@ const config = {
                     }
 
                     // 如果没有可以压缩的了，结束这个进程
-                    if (files.every(file => file.resolved)) {
-                        uploaders.forEach(item => item.kill())
+                    if (files.every(file => file.resolved || file.padding)) {
+                        uploader.kill()
                     }
                 }
                 // key额度用光，换下一个key，重试
